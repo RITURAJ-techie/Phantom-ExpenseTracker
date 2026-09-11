3b7452fd346277823bc58f8349e08c4fa7561de8
RITURAJ-ARENA <majrituraj@gmail.com>
2026-09-12 04:51:05 +0530

    Fix frontend import casing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,19 +6,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/authcontext.jsx";
 
-
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-
         <BrowserRouter>
-
             <AuthProvider>
-
                 <App />
-
             </AuthProvider>
-
         </BrowserRouter>
-
     </StrictMode>
 );
